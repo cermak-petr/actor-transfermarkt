2703779c2f5fc2ff7d6a5d5e79e7d8b08650b9bd
levent91 <zleventcucuk@hotmail.com>
2021-11-22 14:19:22 +0300

    indent

--- main.js
+++ main.js
@@ -192,14 +192,14 @@ async function extractTable(type, page, selector, iColumns, iRowCells) {
             }
         }
         if (type === 'clubplayer') {
-        for (const recordItem of [record]) {
-            const href = await page.$eval(`a[title="${recordItem.player[0]}"]`, anchor => anchor.getAttribute('href'));
-            const routes = href.split('/');
-            const id = routes[routes.length - 1];
-            recordItem.url = `https://www.transfermarkt.com${href}`;
-            recordItem.id = id;
+            for (const recordItem of [record]) {
+                const href = await page.$eval(`a[title="${recordItem.player[0]}"]`, anchor => anchor.getAttribute('href'));
+                const routes = href.split('/');
+                const id = routes[routes.length - 1];
+                recordItem.url = `https://www.transfermarkt.com${href}`;
+                recordItem.id = id;
+            }
         }
-    }
         result.push(record);
     }
     return result;
